7cdba59f14f8b04a5c58c1818c849eaabd6ef425
Ranna Leslie Conceicao Silva <rleslie-@c1r1p4.42sp.org.br>
2023-09-21 14:57:28 -0300

    su

--- testes/chapter_six/main.c
+++ testes/chapter_six/main.c
@@ -18,7 +18,7 @@ void    normal_test2(void)
 
     s = sphere();
     v = normal_at(s, point((sqrt(3) / 3), (sqrt(3) / 3), (sqrt(3) / 3)));
-    printf("x: %f\ny: %f\nz: %f\nw: %f\n", v.x, v.y, v.z, v.w);
+    printf("TESTE 1\nx: %f\ny: %f\nz: %f\nw: %f\n", v.x, v.y, v.z, v.w);
 }
 
 void    normal_translated_test(void)
@@ -26,11 +26,13 @@ void    normal_translated_test(void)
     t_sphere    s;
     t_tuple     n;
 
-    // s = sphere();
-    s = set_transform(s, translation(0, 1, 0));
-    n = normal_at(s, point(0, 1.70711, -0.70711));
-    printf("translated\nx: %f\ny: %f\nz: %f\nw: %f\n", n.x, n.y, n.z, n.w);
+    s = sphere();
+    s = set_transform(s, translation((double)0, (double)1, 0));
+    n = normal_at(s, point((double)0, (double)1.70711, (double)-0.70711));
+    printf("\n\n\n TESTE 1\nx: %lf\ny: %lf\nz: %lf\nw: %lf\n", n.x, n.y, n.z, n.w);
 }
+
+
 
 void    normal_transformed_test(void)
 {
@@ -39,18 +41,16 @@ void    normal_transformed_test(void)
     t_matrices  m;
 
     s = sphere();
-    // m = id_matrix(m);
-    m = mult_matrices(scaling(1, 0.5, 1), rotate_z((float)(PI / 5.0)));
+    m = mult_matrices(scaling((double)1, (double)0.5, (double)1), rotate_z((double)PI/(double)5));
     s = set_transform(s, m);
-    printf("%f\n", sqrt(2.0) / 2.0);
-    n = normal_at(s, point(0, (float)(sqrt(2.0) / 2.0), (float)-(sqrt(2.0) / 2.0)));
-    printf("transformed\nx: %f\ny: %f\nz: %f\nw: %f\n", n.x, n.y, n.z, n.w);
+    n = normal_at(s, point((double)0, (double)(sqrt((double)2) / (double)2), (double)-(sqrt((double)2) / (double)2)));
+    printf("\n\nTESTE 2 transformelf\nx: %lf\ny: %lf\nz: %lf\nw: %lf\n", n.x, n.y, n.z, n.w);
 }
 
 int main()
 {
-    normal_test1();
-    normal_test2();
+    // normal_test1();
+    // normal_test2();
     normal_translated_test();
     normal_transformed_test();
     return (0);
